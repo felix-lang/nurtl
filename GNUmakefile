1d1a49aa6b389c73fe611f2fb8e7e6fe64eb9166
skaller <skaller@users.sourceforge.net>
2021-05-03 17:03:08 +1000

    add convenience macros

---
+++ GNUmakefile
@@ -0,0 +1,9 @@
+help:
+	# Makefile help
+
+test:
+	# CSP TEST
+	clang++ -std=c++17 -ferror-limit=1 -O2 -Isrc/lib src/prog/ex01.cxx -o ex01
+	./ex01
+
+
--- src/lib/sync.hpp
+++ src/lib/sync.hpp
@@ -12,3 +12,30 @@
 #include "svc.hpp"
 #include "sync_sched.hpp"
 
+#define CSP_RETURN {\
+  con_t *tmp = caller;\
+  delete this;\
+  return tmp;\
+}
+
+#define CSP_CALLDEF_START \
+  con_t *call(con_t *caller_a
+
+#define CSP_CALLDEF_MID ){\
+  caller = caller_a;\
+  pc = 0;
+
+#define CSP_CALLDEF_END \
+  return this;\
+}
+
+#define CSP_RESUME_START\
+  con_t *resume() override {\
+  switch(pc){\
+  case 0:
+
+#define CSP_RESUME_END\
+  default: assert(false);\
+  }}
+
+
