d249474c674d8b1203974ffdf9edf2c06a396764
skaller <skaller@users.sourceforge.net>
2021-05-07 14:43:06 +1000

    Add clock.

---
+++ src/lib/clock.hpp
@@ -0,0 +1,132 @@
+struct pqreq_t {
+  double alarm_time;
+  fibre_t *fibre;
+};
+
+// note, reverse order since queue put largest on top
+bool operator < (pqreq_t const &a, pqreq_t const &b) { 
+  return b.alarm_time < a.alarm_time; 
+}
+
+struct csp_clock_t;
+void run_service(csp_clock_t *p);
+
+struct csp_clock_t {
+  chan_epref_t chanepr;
+
+  ::std::mutex lock;
+  ::std::condition_variable cv;
+
+  ::std::priority_queue<pqreq_t> q;
+
+  bool run;
+  ::std::thread thread;
+
+  double now () { 
+    auto t = ::std::chrono::high_resolution_clock().now();
+    auto d = t.time_since_epoch(); // duration
+    return d.count();
+  }
+
+  csp_clock_t () : run(false) { chanepr = make_channel(); start(); }
+  ~csp_clock_t() { ::std::cerr << "Clock destructor" << ::std::endl; stop(); }
+
+  chan_epref_t connect() { return chanepr->dup(); }
+
+private:
+  // puts f back on its active set after timeout
+  void activate_fibre(fibre_t *f) {
+    ::std::cerr << "Activation fibre" << ::std::endl;
+    f->owner->push(f);
+    f->owner->async_count--;
+  }
+
+public:
+  void signal() { cv.notify_one(); }
+
+  void service() {
+    ::std::cerr << ::std::fixed << "Clock service started run flag = " << run << ::std::endl;
+    while(run) {
+      ::std::cerr << "Clock service iteration" << ::std::endl;
+
+      // step 2, read any requests
+      channel_t *chan = chanepr->channel;
+      
+      while(chan->lock.test_and_set(::std::memory_order_acquire)); // spin
+      fibre_t *w = chan->pop_writer();
+      chan->lock.clear(::std::memory_order_release); // release lock
+
+      // move any alarm requests from channel to priority queue
+      while(w) {
+        ::std::cerr << "Got sleep request" << ::std::endl;
+        double **ppalarmat = (double**)w->cc->svc_req->io_request.pdata;
+        ::std::cerr << "Address of data word " << ppalarmat << ::std::endl;
+        double *palarmat = *ppalarmat;
+        ::std::cerr << "data word " << palarmat << ::std::endl;
+        double alarmat = *palarmat; 
+        ::std::cerr << "Sleep request found on channel, alarm at " << alarmat << "!" << ::std::endl;
+        ::std::cerr << "Delay = " << alarmat - now() << " seconds" << ::std::endl;
+        q.push(pqreq_t{alarmat,w});
+
+        while(chan->lock.test_and_set(::std::memory_order_acquire)); // spin
+        w = chan->pop_writer();
+        chan->lock.clear(::std::memory_order_release); // release lock
+      }
+
+      // activate any fibres that have reached alarm time
+      double t = now();
+      ::std::cerr << "Time now is " << t << ::std::endl;
+      double sleep_until = t + 10.0; // one second poll
+      while(!q.empty()) {
+        ::std::cerr << "Examining queue" << ::std::endl;
+        pqreq_t top = q.top();
+        if(top.alarm_time > t) { 
+           sleep_until = top.alarm_time; 
+           ::std::cerr << "UNEXPRIRED TIMER" << ::std::endl;
+           break; 
+        }
+        q.pop();
+        ::std::cerr << "EXPRIRED TIMER for fibre " << top.fibre << ::std::endl;
+        activate_fibre(top.fibre);
+      }
+
+      // sleep
+      ::std::cerr << "Sleep until " << sleep_until << ", for " << sleep_until - now() << ::std::endl;
+      {
+        ::std::unique_lock lk(lock); // lock mutex
+        auto t = ::std::chrono::time_point<
+           ::std::chrono::high_resolution_clock,
+           ::std::chrono::duration<double>> ( 
+           ::std::chrono::duration<double>(sleep_until));
+
+        cv.wait_until(lk, t);
+        // mutex is lock on exit from wait but then released by RAII
+       }
+       ::std::cerr << "Condition variable woke up" << ::std::endl;
+    } // infinite loop
+  } // service
+   
+  void start() {
+    ::std::cerr << "Start clock, flag = " << run << ::std::endl;
+    if(run) return; // already running
+    run = true;
+    ::std::cerr << "spawning thread, flag = " << run << ::std::endl;
+    thread = ::std::thread(run_service, this); // start thread
+    ::std::cerr << "thread spawned, flag = " << run << ::std::endl;
+  }
+  void stop () {
+    ::std::cerr << "Stop clock flag = " << run << ::std::endl;
+    if(run) {
+      run = false;
+      thread.join();
+    }
+  }
+};
+
+void run_service(csp_clock_t *p) { p->service(); }
+
+// makes a clock and starts it running
+// the clock dies when the last reference is lost
+::std::shared_ptr<csp_clock_t> make_clock() { 
+  return ::std::make_shared<csp_clock_t>();
+}
