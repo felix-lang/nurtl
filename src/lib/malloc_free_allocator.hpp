ab95df085275f4019243c76bfda2e4f858299a3d
skaller <skaller@users.sourceforge.net>
2021-05-28 14:33:20 +1000

    Debugs

--- src/lib/malloc_free_allocator.hpp
+++ src/lib/malloc_free_allocator.hpp
@@ -1,6 +1,6 @@
 
 struct malloc_free_allocator_t : allocator_t {
-  void *allocate(size_t n) override { return malloc(n); }
+  void *allocate(size_t n) override { auto p = malloc(n); ::std::cerr << "malloc(" << n << ") = " << p << ::std::endl; return p; }
   void deallocate(void *p, size_t n) override { free(p); }
   ~malloc_free_allocator_t() override {}
 };
--- src/lib/wait_free_allocator.hpp
+++ src/lib/wait_free_allocator.hpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 
 // client request entry: client needs n_blocks of size block_size
 struct mem_req_t {
@@ -95,9 +96,12 @@ static size_t calringbuffernentries(size_t v) {
 
 wait_free_allocator_t::wait_free_allocator_t(allocator_t *a, ::std::vector<mem_req_t> reqs) : allocator(a) {
 
+  ::std::cerr << ::std::setbase(16);
+
   // find the largest block requested
   size_t max_block_size = 0;
   for(auto req: reqs) max_block_size = ::std::max(req.block_size, max_block_size);
+  ::std::cerr << "max_block_size " << max_block_size << ::std::endl;
 
   // make an array mapping block size to request count
   ::std::vector<size_t> nblocks;
@@ -105,29 +109,39 @@ wait_free_allocator_t::wait_free_allocator_t(allocator_t *a, ::std::vector<mem_r
     nblocks.push_back(0);
   for (auto req: reqs) 
     nblocks[calblocksize(req.block_size)] += req.n_blocks;
+  
+  ::std::cerr << "nblocks array: " << ::std::endl;
+  for(size_t k = 0; k<= max_block_size; ++k)
+    ::std::cerr << "  " << k  << ": " << nblocks[k] << ::std::endl;
 
   // find user memory requirement
   size_t user_memory = 0;
   for (size_t i = 0; i <= max_block_size; ++i) 
     user_memory += i * nblocks[i]; 
+  ::std::cerr << "user_memory: " << user_memory << ::std::endl;
 
   // calculate the store required for ring buffers
   size_t ring_buffer_memory = 0;
   for (size_t i = 0; i <= max_block_size; ++i) 
     ring_buffer_memory += calringbuffernentries(nblocks[i]) * sizeof(void*); 
+  ::std::cerr << "ring_buffer_memory: " << ring_buffer_memory << ::std::endl;
 
   // calculate store required for ring buffer objects
   size_t ring_buffer_object_memory = 0;
   for (size_t i = 0; i <= max_block_size; ++i) 
     ring_buffer_object_memory +=  (nblocks[i] == 0 ? 0 : sizeof(wait_free_ring_buffer_t));
+  ::std::cerr << "ring_buffer_object_memory: " << ring_buffer_object_memory << ::std::endl;
 
   // calculate store for ring_buffer_pointers
-  size_t ring_buffer_pointer_memory = max_block_size * sizeof(wait_free_ring_buffer_t*);
+  size_t ring_buffer_pointer_memory = (max_block_size + 1) * sizeof(wait_free_ring_buffer_t*);
+  ::std::cerr << "ring_buffer_pointer_memory: " << ring_buffer_object_memory << ::std::endl;
 
   // total store requirement
   arena_size = user_memory + ring_buffer_memory + ring_buffer_object_memory + ring_buffer_pointer_memory;
+  ::std::cerr << "arena_size: " << arena_size<< ::std::endl;
 
-  arena = (unsigned char*)allocator->allocate (arena_size);
+  arena = (unsigned char*)(allocator->allocate (arena_size));
+  ::std::cerr << "arena: " << (void*)arena << ::std::endl;
 
   unsigned char *arena_pointer = arena;
 
